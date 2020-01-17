79c054a8a69c2fb4dfdaf32cba198df3a55b7c4e
Diego Felipe Garcia Cortes <difgar@gmail.com>
2020-01-17 17:45:23 -0500

    Implementando la libreria Morgan como para la generacion del access.log

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const app = express();
-
 const { config } = require('./config/index');
 const usersApi = require('./router/users');
 const { logErrors, wraperError, errorHanlder } = require('./utils/middleware/errorHandler');
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+
+const app = express();
+
+// create a rotating write stream
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
+})
+
+//Logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 //Body parser
 app.use(express.json());
